9ccef1be1661d8da6d304961f2d085f22dea040c
Nathan Wondim <nathanwondim16@gmail.com>
2026-04-15 19:19:04 -0700

    Added a generate report feature and changed while loop to a do/while loop

--- src/main/java/CellPhoneApplication.java
+++ src/main/java/CellPhoneApplication.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class CellPhoneApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         // Creating phone object
         CellPhone cellPhone1 = new CellPhone();
@@ -16,20 +16,19 @@ public class CellPhoneApplication {
 
         // Prompting user to enter in the model of their phone
         System.out.print("What model is the phone? ");
-        cellPhone1.setModel(scanner.nextLine());
+        cellPhone1.setModel(scanner.nextLine().strip().replaceAll("\\s+", " "));
 
 
         // Prompting user to enter in their carrier
         System.out.print("Who is the carrier? ");
-        cellPhone1.setCarrier(scanner.nextLine());
+        cellPhone1.setCarrier(scanner.nextLine().strip());
 
 
         // Prompting user to enter in their phone number
-        System.out.print("What is the phone number? ");
         String number;
-
-        while(true) {
-            number = scanner.nextLine();
+        do {
+            System.out.print("Please enter in the phone number: ");
+            number = scanner.nextLine().strip();
             if(number.matches("\\d{3}-\\d{3}-\\d{4}")) {
                 cellPhone1.setPhoneNumber(number);
                 System.out.println(); // New line for space
@@ -37,59 +36,53 @@ public class CellPhoneApplication {
             }
             System.out.println("\nEntered in phone number using incorrect format");
             System.out.println("Format required -> xxx-xxx-xxxx");
-            System.out.print("Please enter in the phone number using the correct format: ");
 
-        }
+        } while(true);
+
+
 
         // Prompting user to enter in the owner of the phone
         System.out.print("Who is the owner of the phone? ");
-        cellPhone1.setOwner(scanner.nextLine());
+        cellPhone1.setOwner(scanner.nextLine().strip().replaceAll("\\s+", " "));
 
         // Setting cellPhone2 information
         cellPhone2.setPhoneNumber("855-555-2222");
         cellPhone2.setOwner("Jake from State Farm");
         System.out.println();
 
+        // Generating customer's information
+        for(int i = 0; i < 100; i++) {
+            System.out.print("\rGenerating customer report " + i + "███▒▒▒▒▒▒▒");
+            Thread.sleep(35);
+        }
+        System.out.println("\rDone\n");
 
+
+        // Displaying user's phone info
         display(cellPhone1);
         display(cellPhone2);
 
 
+        // Dialing phone numbers
         cellPhone2.dial(cellPhone1.getPhoneNumber());
         cellPhone1.dial(cellPhone2.getPhoneNumber());
 
+        // Creating another CellPhone object after adding constructors
+        CellPhone cellPhone3 = new CellPhone(2893238,"iPhone 15 Pro Max", "T-Mobile","425-849-2794","Hubert Johnson");
+        display(cellPhone3);
 
     }
 
+    // Displays a summary of user's phone information
     public static void display(CellPhone phone) {
-        System.out.println("\n\n" + phone.getOwner() + "'s information: ");
+        System.out.println();
+        System.out.println(phone.getOwner() + "'s information: ");
+        System.out.println("----------------------------------------------");
         System.out.println("Serial number: " + phone.getSerialNumber());
         System.out.println("Phone Model: " + phone.getModel());
         System.out.println("Carrier: " + phone.getCarrier());
         System.out.println("Phone number: " + phone.getPhoneNumber());
         System.out.println("Owner: " + phone.getOwner());
-
-        // Still working on this code so i've commented it out for now.
-        /*
-        // Printing out user's details and asking for confirmation on details.
-        System.out.println("\n\nConfirm details: ");
-        System.out.println("----------------------------");
-
-
-        System.out.println("\nDoes everything above look correct");
-        System.out.print("Answer: Yes or No -> ");
-        String confirm = scanner.nextLine();
-        if(confirm.equalsIgnoreCase("yes")) {
-            System.out.println("\nGreat!");
-        } else if (confirm.equalsIgnoreCase("no")) {
-            System.out.println("What details did we get wrong?");
-            String error = scanner.nextLine(); // Add more code that actually uses this
-            System.out.println("\nSorry about that");
-            System.out.println("Please restart the application and enter in the correct information again");
-        } else {
-            throw new IllegalArgumentException("Please only answer yes or no next time");
-        }
-         */
+        System.out.println("----------------------------------------------");
     }
-
 }
